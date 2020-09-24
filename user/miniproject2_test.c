90c04860d0a540746d771096932fee34740288e3
Bryce Palmer <everettraven@gmail.com>
2020-09-24 17:28:13 -0400

    test again

--- user/miniproject2_test.c
+++ user/miniproject2_test.c
@@ -8,35 +8,38 @@
 int main(int argc, char *argv[])
 {
     int child1 = 1, child2 = 1;
-   // Create two children to run the same task, but change their ticket counts
-   child1 = fork();
-   if (child1 > 0)
-   {
-        child2 = fork();
-   }
+    // Create two children to run the same task, but change their ticket counts
+    child1 = fork();
+    if (child1 > 0)
+    {
+            child2 = fork();
+    }
 
-   if(child1 < 0 && child2 > 0)
-   {
-        int i;
-        int pid = getpid();
-        changetickets(pid, 3);
-        for(i = 0; i < 10; i++)
-        {
-            printf(1,"I am child1 printing: %d", i);
-        }
-   }
-   else if(child1 > 0 && child2 < 0)
-   {
-        int j;
-        int pid = getpid();
-        changetickets(pid, 100);
-        for(j = 0; j < 10; j++)
-        {
-            printf(1,"I am child2 printing: %d", j);
-        }
-   }
+    if(child1 < 0 && child2 > 0)
+    {
+            int i;
+            int pid = getpid();
+            changetickets(pid, 3);
+            for(i = 0; i < 10; i++)
+            {
+                printf(1,"I am child1 printing: %d", i);
+            }
+    }
+    else if(child1 > 0 && child2 < 0)
+    {
+            int j;
+            int pid = getpid();
+            changetickets(pid, 100);
+            for(j = 0; j < 10; j++)
+            {
+                printf(1,"I am child2 printing: %d", j);
+            }
+    }
 
-    wait();
-    wait();
-    exit();
+    else{
+        wait();
+        wait();
+        exit();
+    }
+
 }
